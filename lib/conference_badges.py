a9b28c0671650d149b4c8ed7989d53bdff81e9dc
Charlie <cnewlinds@gmail.com>
2023-11-29 22:11:32 +1100

    test pass

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    list_of_names = []
+    for name in names:
+        list_of_names.append(badge_maker(name))
+    return list_of_names
+    
 
 def assign_rooms(names):
-    return None
+    welcome_messages = []
+    room = 1 
+    for name in names: 
+        welcome_messages.append(f"Hello, {name}! You'll be assigned to room {room}!")
+        room += 1
+    return(welcome_messages) 
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for room in rooms:
+        print(room)
+
+
+print(printer(["Guido", "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]))
